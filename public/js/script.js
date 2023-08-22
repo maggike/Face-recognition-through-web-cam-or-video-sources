214d286b7d7c44ee799d251f4a5bdf56de19effd
maggike <meghana6400nms@gmail.com>
2023-08-21 21:55:01 -0400

    api added, script working on
    script added

---
+++ public/js/script.js
@@ -0,0 +1,72 @@
+const video=document.getElementById('videoInput')
+Promise.all([
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+]).then(start)
+
+function start() {
+    document.body.append('Models Loaded')
+    ///navigator.getUserMedia(
+    //    {video:{}},
+    //    stream=>video.srcObject=stream,
+    //    err=>console.error(err)
+
+    //)
+    video.src = '../videos/speech.mp4'
+    console.log('video added')
+
+    recognizeFaces()
+}
+
+async function recognizeFaces(){
+
+    const labeledDescriptors = await loadLabeledImages()
+    const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors,0.7)
+    video.addEventListener('play',async()=>{
+        console.log('playing')
+        const canvas = faceapi.createCanvasFromMedia(video)
+        document.body.append(canvas)
+
+        const displaySize={width:video.width,height:video.height}
+        faceapi.matchDimensions(canvas, displaySize)
+
+        setInterval(async()=>{
+            const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
+
+            const resizedDetections=faceapi.resizeResults(detections, displaySize)
+            canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height)
+
+            const results=resizedDetections.map((d)=>{
+                return faceMatcher.findBestMatch(d.descriptor)
+
+            })
+            results.forEach((result,i)=>{
+                const box=resizedDetections[i].detection.box
+                const drawBox = new faceapi.draw.DrawBox(box, {label: result.toString()})
+                drawBox.draw(canvas)
+            })
+            }, 100)
+
+        })
+    
+}
+
+function loadLabeledImages(){
+    const labels= ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawk Eye', 'Iron Man', 'Jim Rhodes', 'Thor', 'Tony Stark']
+    //const labels= ['Meghana Kesanapalli']
+
+    return Promise.all(
+        labels.map(async (label)=>{
+            const descriptions=[]
+            for(let i=1; i<=2; i++){
+                const img=await faceapi.fetchImage(`../labeled_images/${label}/${i}.jpg`)
+                const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+                descriptions.push(detections.descriptor)
+
+            }
+            document.body.append(label + 'Faces Loaded|')
+            return new faceapi.LabeledFaceDescriptors(label,descriptions)
+        })
+    )
+}
